97dfff4a91f777b41779ed17a5a04d29ecdb1ec2
ANU7MADHAV <anumadhavan888@gmail>
2024-02-07 09:53:08 +0530

    fixed jwt token

--- app/api/(userapi)/signout/route.ts
+++ app/api/(userapi)/signout/route.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export const GET = () => {
-  cookies().delete("token" || "_vercel_jwt");
+  cookies().delete("token");
 
   return NextResponse.json({
     message: "Cookies deleted succesfully",
--- utilities/isAuthenticated.ts
+++ utilities/isAuthenticated.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from "next/server";
 
 const isAuthenticated = (request: NextRequest): boolean => {
-  const token = request.cookies.get("token" && "_vercel_jwt")?.value;
+  const token = request.cookies.get("token")?.value;
   if (token) {
     return true;
   } else {
