184c69e33eacec433dfad375b9fb662bf4e26976
ANU7MADHAV <anumadhavan888@gmail>
2024-02-07 20:25:24 +0530

    expires error

--- utilities/tokenGenerate.ts
+++ utilities/tokenGenerate.ts
@@ -2,6 +2,6 @@ import jwt from "jsonwebtoken";
 
 export function generateAccessToken(userId: string) {
   return jwt.sign({ userId }, process.env.TOKEN_STORE!, {
-    expiresIn: "60 * 60 * 24s",
+    expiresIn: "1d",
   });
 }
