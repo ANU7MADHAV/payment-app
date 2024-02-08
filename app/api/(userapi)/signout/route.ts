43bca2023abac4faab6fa4367ca13dbef91fdfb9
ANU7MADHAV <anumadhavan888@gmail>
2024-02-08 09:24:49 +0530

    small change

--- app/api/(userapi)/signout/route.ts
+++ app/api/(userapi)/signout/route.ts
@@ -3,29 +3,29 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const user = getUser(req);
-  if (!user) {
-    console.log("No user");
-    return NextResponse.json({ message: "No user details" });
-  }
+  // const user = getUser(req);
+  // if (!user) {
+  //   console.log("No user");
+  //   return NextResponse.json({ message: "No user details" });
+  // }
 
-  const userId = (user as { userId: string }).userId;
+  // const userId = (user as { userId: string }).userId;
 
-  const offlineUser = await prisma?.user.update({
-    where: {
-      id: userId,
-    },
-    data: {
-      online: false,
-    },
-  });
+  // const offlineUser = await prisma?.user.update({
+  //   where: {
+  //     id: userId,
+  //   },
+  //   data: {
+  //     online: false,
+  //   },
+  // });
 
-  if (!offlineUser) {
-    return NextResponse.json({
-      message: "failed updated user offline",
-      status: 400,
-    });
-  }
+  // if (!offlineUser) {
+  //   return NextResponse.json({
+  //     message: "failed updated user offline",
+  //     status: 400,
+  //   });
+  // }
   cookies().delete("token");
 
   return NextResponse.json({
