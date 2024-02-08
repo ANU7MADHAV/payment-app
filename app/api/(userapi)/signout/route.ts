26a75e284629ac48e197936cc3485adea3e7873d
ANU7MADHAV <anumadhavan888@gmail>
2024-02-08 09:37:50 +0530

    fixed signout issue prisma undefined

--- app/api/(userapi)/signout/route.ts
+++ app/api/(userapi)/signout/route.ts
@@ -1,35 +1,37 @@
 import { getUser } from "@/utilities/getUser";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/utilities/db";
 
 export async function GET(req: NextRequest) {
-  // const user = getUser(req);
-  // if (!user) {
-  //   console.log("No user");
-  //   return NextResponse.json({ message: "No user details" });
-  // }
+  const user = getUser(req);
+  if (!user) {
+    console.log("No user");
+    return NextResponse.json({ message: "No user details" });
+  }
 
-  // const userId = (user as { userId: string }).userId;
+  const userId = (user as { userId: string }).userId;
 
-  // const offlineUser = await prisma?.user.update({
-  //   where: {
-  //     id: userId,
-  //   },
-  //   data: {
-  //     online: false,
-  //   },
-  // });
-
-  // if (!offlineUser) {
-  //   return NextResponse.json({
-  //     message: "failed updated user offline",
-  //     status: 400,
-  //   });
-  // }
-  cookies().delete("token");
-
-  return NextResponse.json({
-    message: "Cookies deleted succesfully",
-    status: 200,
+  const offlineUser = await prisma?.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      online: false,
+    },
   });
+
+  if (!offlineUser) {
+    return NextResponse.json({
+      message: "failed updated user offline",
+      status: 400,
+    });
+  }
+  if (offlineUser) {
+    cookies().delete("token");
+    return NextResponse.json({
+      message: "Cookies deleted succesfully",
+      status: 200,
+    });
+  }
 }
