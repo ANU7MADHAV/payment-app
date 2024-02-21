8e68b63fd8cf781a464e3ec09b6d6f489fcbdbcb
ANU7MADHAV <anumadhavan888@gmail>
2024-02-21 09:26:28 +0530

    almost completed

--- components/ExitButton.tsx
+++ components/ExitButton.tsx
@@ -5,7 +5,7 @@ const ExitButton = () => {
   return (
     <div>
       <Link href="/">
-        <button className="flex justify-end p-4 text-xl text-red-500">
+        <button className="text-xl text-red-500">
           <RxExit />
         </button>
       </Link>
